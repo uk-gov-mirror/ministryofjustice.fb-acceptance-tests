7baf5569da8d9f8423b9a339ac603ec58885c346
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-03-10 17:22:10 +0000

    Add status description to save and return mock app

--- mock-apps/save-and-return-simulator/app.rb
+++ mock-apps/save-and-return-simulator/app.rb
@@ -3,18 +3,25 @@ require 'json'
 require 'sinatra/json'
 require 'pry'
 
+EMAIL_CONTENT_FILE = 'tmp/email_content.json'
+
 post '/email' do
   data = request.body.read
   parsed_data = JSON.generate(JSON.parse(data))
-  if File.write('tmp/params.json', parsed_data)
-    status 200
+  if File.write(EMAIL_CONTENT_FILE, parsed_data)
+    status :ok
     body "Ok"
   else
-    status 400
+    status :bad_request
     body "Unable to generate params"
   end
 end
 
 get '/email' do
-  json JSON.parse(File.read('tmp/params.json'))
+  begin
+    email_content = File.read(EMAIL_CONTENT_FILE)
+    json JSON.parse(email_content)
+  rescue
+    status :not_found
+  end
 end
