e4956fb8dedd27bca7f36c334735b184bb0c7e46
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-03-10 15:37:51 +0000

    Ignoreing tmp folder

--- mock-apps/save-and-return-simulator/test/request.sh
+++ mock-apps/save-and-return-simulator/test/request.sh
@@ -3,6 +3,6 @@ set -x
 curl -i \
     -H "Accept: application/json" \
     -X POST -d '{ "message": { "to": "user@example.com", "subject": "subject goes here", "body": "body as string goes here","template_name": "name-of-template","[extra_personalisation]": {  "token": "my-token" }}}' \
-    http://localhost:3005/email
+    http://localhost:8080/email
 
-curl -i -H "Accept: application/json" -X GET http://localhost:3005/email
+curl -i -H "Accept: application/json" -X GET http://localhost:8080/email
