20b5cef13920db2128261622c21f6c769699aabc
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-05-26 17:06:30 +0100

    Add tests for date validation.
    Make sure that validation of dates is happen automatically.
    This will pass after the following PR is merged:
    https://github.com/ministryofjustice/fb-runner-node/pull/588

--- integration/spec/support/pages/components_date_app.rb
+++ integration/spec/support/pages/components_date_app.rb
@@ -1,3 +1,13 @@
 class ComponentsDateApp < ServiceApp
   set_url ENV.fetch('COMPONENTS_DATE_APP')
+
+  element :day_field, :field, 'Day'
+  element :month_field, :field, 'Month'
+  element :year_field, :field, 'Year'
+
+  elements :error_messages, '.govuk-error-summary__list'
+
+  def error_summary
+    self.error_messages.map(&:text)
+  end
 end
--- integration/spec/support/service_app.rb
+++ integration/spec/support/service_app.rb
@@ -1,4 +1,7 @@
 class ServiceApp < SitePrism::Page
+  element :start_button, :button, 'Start'
+  element :continue_button, :button, 'Continue'
+
   def load(expansion_or_html = {}, &block)
     puts "Visiting form: #{self.url}"
     super
