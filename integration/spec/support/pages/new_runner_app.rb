6a5c6025f3c0d0e682e3d7cae883aef8d556235b
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-04-08 07:57:22 -0300

    Add expectations to check your answers page

--- integration/spec/support/pages/new_runner_app.rb
+++ integration/spec/support/pages/new_runner_app.rb
@@ -2,4 +2,5 @@ class NewRunnerApp < FeaturesEmailApp
   set_url ENV.fetch('NEW_RUNNER_APP')
 
   element :start_button, :button, 'Start now'
+  element :yes_field, :radio_button, 'Yes', visible: false
 end
