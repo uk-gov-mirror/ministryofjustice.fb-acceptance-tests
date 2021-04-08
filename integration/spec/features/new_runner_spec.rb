a9d8b4060dd2f3a9200435605dc3c4b11deedf63
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-04-08 15:31:46 -0300

    Create inbox object and separate behavior
    For the new runner we need to search the exact email in attachments
    because the new runner doesn't concatanate fields into the subject field
    
    And into the classic/old runner we need to find by subject.

--- integration/spec/features/new_runner_spec.rb
+++ integration/spec/features/new_runner_spec.rb
@@ -1,15 +1,17 @@
+require 'pdf-reader'
+
 describe 'New Runner' do
   let(:form) { NewRunnerApp.new }
   before :each do
     OutputRecorder.cleanup_recorded_requests if ENV['CI_MODE'].blank?
   end
-  let(:generated_first_name) { SecureRandom.uuid }
+  let(:generated_name) { "FN-#{SecureRandom.uuid}" }
 
   it 'sends an email with the submission in a PDF' do
     form.load
     form.start_button.click
     form.first_name_field.set('Stormtrooper')
-    form.last_name_field.set('FN-2187')
+    form.last_name_field.set(generated_name)
     continue
 
     form.email_field.set('fb-acceptance-tests@digital.justice.gov.uk')
@@ -41,7 +43,7 @@ describe 'New Runner' do
 
     # check your answers
     expect(page.text).to include('First name Stormtrooper')
-    expect(page.text).to include('Last name FN-2187')
+    expect(page.text).to include("Last name #{generated_name}")
     expect(page.text).to include('Your email address fb-acceptance-tests@digital.justice.gov.uk')
     expect(page.text).to include('Your cat My cat is a fluffy killer £ % ~ ! @ # $ ^ * ( ) - _ = + [ ] | ; , . ?')
     expect(page.text).to include("Your fruit Apples\nPears")
@@ -53,39 +55,35 @@ describe 'New Runner' do
 
     expect(form.text).to include("You've sent us the answers about your cat!")
 
-    attachments = find_attachments(
-      id: 'Submission from New Acceptance tests Email Output',
-      pdf_filename: '-answers.pdf',
-      user_attachment_filename: 'hello_world.txt'
-    )
+    attachments = find_attachments(id: generated_name)
 
-    assert_pdf_contents(attachments[:pdf_answers])
+    assert_pdf_contents(attachments)
   end
 
   def continue
     form.continue_button.click
   end
 
-  def parse_email(email)
-    url_decoded_hash = CGI::parse(email)
-    Mail.read_from_string(url_decoded_hash.fetch('raw_message'))
-  end
-
-  def assert_pdf_contents(attachment)
-    pdf_path = '/tmp/submission.pdf'
-
-    File.open(pdf_path, 'w') { |file| file.write(attachment) }
-    result = PDF::Reader.new(pdf_path).pages.map { |page| page.text }.join(' ')
-
+  def assert_pdf_contents(attachments)
+    pdf_path = "/tmp/submission-#{SecureRandom.uuid}.pdf"
+    File.open(pdf_path, 'w') do |file|
+      file.write(attachments[:pdf_answers])
+    end
+    result = PDF::Reader.new(pdf_path).pages.map do |page|
+      page.text
+    end.join(' ')
     p 'Asserting PDF contents'
 
     expect(result).to include('Subheading for new acceptance tests')
-    expect(result).to include('Submission for New Acceptance tests Email Output')
+    expect(result).to include(
+      "Submission for New Acceptance tests Email"
+    )
 
     # text
-    expect(result).to include('Your name')
+    # Not possible to test this at the moment as we are holding sections feature
+    # expect(result).to include('Your name')
     expect(result).to match(/First name[\n\r\s]+Stormtrooper/)
-    expect(result).to match(/Last name[\n\r\s]+FN-2187/)
+    expect(result).to match(/Last name[\n\r\s]+#{generated_name}/)
 
     # email
     expect(result).to include('Your email address')
@@ -93,7 +91,7 @@ describe 'New Runner' do
 
     # textarea
     expect(result).to include('Your cat')
-    expect(result).to include('My cat is a fluffy killer named £ % ~ ! @ # $ ^ * ( ) - _ = + [ ] | ; , . ?')
+    expect(result).to include('My cat is a fluffy killer £ % ~ ! @ # $ ^ * ( ) - _ = + [ ] | ; , . ?')
 
     # checkbox
     expect(result).to include('Your fruit')
@@ -101,44 +99,23 @@ describe 'New Runner' do
     expect(result).to include('Pears')
 
     # date
-    expect(result).to include('When did your cat choose you?')
+    expect(result).to include("When did your cat choose\n you?")
     expect(result).to include('12 November 2007')
 
     # number
-    expect(result).to include('How many cats have chosen you?')
+    expect(result).to include("How many cats have chosen\n you?")
     expect(result).to include('28')
   end
 
-  def find_attachments(id:, pdf_filename:, user_attachment_filename:)
+  def find_attachments(id:)
     if ENV['CI_MODE'].present?
       EmailAttachmentExtractor.find(
         id: id,
-        pdf_filename: pdf_filename,
-        user_attachment_filename: user_attachment_filename,
-        expected_emails: 1
+        expected_emails: 1,
+        find_criteria: :attachments
       )
     else
-      all_attachments = {}
-
-      emails = OutputRecorder.wait_for_result(url: '/email', expected_requests: 4)
-      emails[0..1].each do |email|
-        attachments = parse_email(email).attachments
-        pdf_answers = attachments.detect do |attachment|
-          attachment.filename.include?(pdf_filename)
-        end
-        file_upload = attachments.detect do |attachment|
-          attachment.filename.include?(user_attachment_filename)
-        end
-
-        all_attachments[:pdf_answers] = pdf_answers.decoded
-        all_attachments[:file_upload] = file_upload.decoded
-      end
-
-      all_attachments[:csvs] = emails[2..3].map do |email|
-        parse_email(email).attachments.map(&:decoded)
-      end.flatten
-
-      all_attachments
+      {}
     end
   end
 end
--- integration/spec/support/email_output_service/email_attachment_extractor.rb
+++ integration/spec/support/email_output_service/email_attachment_extractor.rb
@@ -1,18 +1,17 @@
 class EmailAttachmentExtractor
-  def self.find(id:, pdf_filename:, user_attachment_filename:, expected_emails: nil)
-    email_finder = EmailFinder.new(
-      service: GoogleService,
-      id: id,
-      pdf_filename: pdf_filename,
-      user_attachment_filename: user_attachment_filename
-    )
-
-    email_finder.expected_emails = expected_emails if expected_emails.present?
-
+  def self.find(
+    id:,
+    expected_emails: nil,
+    find_criteria: nil
+  )
     tries = 1
     max_tries = 20
 
     until tries > max_tries
+      email_finder = EmailFinder.new(id: id, service: GoogleService)
+      email_finder.expected_emails = expected_emails if expected_emails.present?
+      email_finder.find_criteria = find_criteria if find_criteria.present?
+
       if email_finder.email_received?
         break
       else
@@ -25,7 +24,9 @@ class EmailAttachmentExtractor
     if tries == max_tries || !email_finder.email_received?
       raise "Email '#{email_finder.id}' not found"
     else
-      email_finder.attachments
+      email_finder.attachments.tap do
+        email_finder.remove_emails
+      end
     end
   end
 end
